195b4a4bbcca4de5c0e2334d89ea19438f7fd196
Rizky Hafitsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-06-18 22:58:16 +0700

    Add files via upload

---
+++ src/printingdasar_java/PrintingDasar_Java.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package printingdasar_java;
+
+/**
+ *
+ * @author RizkyKhapidsyah
+ */
+public class PrintingDasar_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama Kamu Siapa?");         //println   = mencetak tulisan ke layar + enter
+        System.out.print("Nama Saya Anu \n");           //print     = mencetak tulisan ke layar tanpa enter. \n = perintah untuk menambahkan enter
+        System.out.printf("Umur Saya %d tahun.", 90);       //printf    = disebut juga 'print format': mencetak tulisan ke layar dengan menggunakan assigment format. (%d = decimal)
+    }
+}
